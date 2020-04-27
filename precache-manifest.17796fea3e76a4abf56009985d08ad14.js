cd1659faa44ac14494919adede807cd3e1f0add3
Kartik Verma <kvs1297@gmail.com>
2020-04-28 02:27:07 +0530

    Updates

---
+++ precache-manifest.17796fea3e76a4abf56009985d08ad14.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e9b9f569882b138f7c2f4d52e5eb81",
+    "url": "/graphy/index.html"
+  },
+  {
+    "revision": "a54c4dbbb6b47ad5e65f",
+    "url": "/graphy/static/css/main.886b1348.chunk.css"
+  },
+  {
+    "revision": "ffbf7a2cc93dd0909256",
+    "url": "/graphy/static/js/2.4b9297ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/graphy/static/js/2.4b9297ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a54c4dbbb6b47ad5e65f",
+    "url": "/graphy/static/js/main.d7b6c2a5.chunk.js"
+  },
+  {
+    "revision": "48f4c82d195b8d477ded",
+    "url": "/graphy/static/js/runtime-main.e59d2d1e.js"
+  },
+  {
+    "revision": "66a8f0126d0e5a8fab91afbd45c17eba",
+    "url": "/graphy/static/media/01 Digit.66a8f012.ttf"
+  },
+  {
+    "revision": "964111990ca3937960147b80e8d07e61",
+    "url": "/graphy/static/media/ARCADECLASSIC.96411199.TTF"
+  },
+  {
+    "revision": "7676beb2b1f19eec709feed096327d21",
+    "url": "/graphy/static/media/AlfaSlabOne-Regular.7676beb2.ttf"
+  },
+  {
+    "revision": "e9ee108cd098321ac27b95ebcebfcdf6",
+    "url": "/graphy/static/media/Audiowide-Regular.e9ee108c.ttf"
+  },
+  {
+    "revision": "6f6daa599c68f169785a5e9fb82fa55a",
+    "url": "/graphy/static/media/BUBBLEBATH.6f6daa59.ttf"
+  },
+  {
+    "revision": "a82040ecf06139df2f3e653326b3420c",
+    "url": "/graphy/static/media/Bangers-Regular.a82040ec.ttf"
+  },
+  {
+    "revision": "bc0d30551f2a2318acbcee074581c904",
+    "url": "/graphy/static/media/BlackOpsOne-Regular.bc0d3055.ttf"
+  },
+  {
+    "revision": "dc97f34ade1b590e1688b6803b1a1f03",
+    "url": "/graphy/static/media/CabinSketch-Regular.dc97f34a.ttf"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/graphy/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "249f9330482c8b6305b62435c8adfcf2",
+    "url": "/graphy/static/media/DisposableDroidBB.249f9330.ttf"
+  },
+  {
+    "revision": "9b48452cfcc1da9b1f684f991cb3d1e3",
+    "url": "/graphy/static/media/GochiHand-Regular.9b48452c.ttf"
+  },
+  {
+    "revision": "c6004be49628b1226eb31b068348d24f",
+    "url": "/graphy/static/media/LuckiestGuy-Regular.c6004be4.ttf"
+  },
+  {
+    "revision": "411051cb61bcda5517943601e8734cea",
+    "url": "/graphy/static/media/Monoton-Regular.411051cb.ttf"
+  },
+  {
+    "revision": "314cfaceddaeff5e81e79d1a34b3b1fa",
+    "url": "/graphy/static/media/Oxanium-Regular.314cface.ttf"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/graphy/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "80520f263d966b867eaf5f8ac5feac0a",
+    "url": "/graphy/static/media/Pixel.80520f26.ttf"
+  },
+  {
+    "revision": "6c95fa96859384b5e470c92bf331a525",
+    "url": "/graphy/static/media/PixelFJVerdana12pt.6c95fa96.ttf"
+  },
+  {
+    "revision": "2c404fd06cd67770807d242b2d2e5a16",
+    "url": "/graphy/static/media/PressStart2P-Regular.2c404fd0.ttf"
+  },
+  {
+    "revision": "60ad0534c4f6733fca1d6246d200b10c",
+    "url": "/graphy/static/media/SFPixelate.60ad0534.ttf"
+  },
+  {
+    "revision": "7dc316626726a6192011df8351950103",
+    "url": "/graphy/static/media/SUBWT___.7dc31662.ttf"
+  },
+  {
+    "revision": "5d6acef1af47457f92ef99414c089dd4",
+    "url": "/graphy/static/media/dotty.5d6acef1.ttf"
+  },
+  {
+    "revision": "18abc72225d23f9393a4bddfe0c9351c",
+    "url": "/graphy/static/media/joystix monospace.18abc722.ttf"
+  },
+  {
+    "revision": "115edbd61c79da3971b311bc8f4bdc0e",
+    "url": "/graphy/static/media/sayso chic.115edbd6.ttf"
+  }
+]);
